1abd7c8f1195591fd6cbb2c8951e2d8d8b1f1bfc
Do Young <64531572+kimdoyoung1110@users.noreply.github.com>
2026-03-17 22:21:27 +0900

    [level 0] Title: 문자열 출력하기, Time: 180.16 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String hello = scanner.nextLine();
+        System.out.print(hello);
+    }
+}
