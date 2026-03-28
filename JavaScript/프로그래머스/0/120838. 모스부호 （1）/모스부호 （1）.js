22b39f0754841dd0aadda746cff8d6a682f98038
Do Young <64531572+kimdoyoung1110@users.noreply.github.com>
2026-03-28 15:29:14 +0900

    [level 0] Title: 모스부호 (1), Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    };
+    const arr = letter.split(" ");
+    let result = "";
+    
+    for(let code of arr){
+        result += morse[code];
+    }
+    return result;
+}
