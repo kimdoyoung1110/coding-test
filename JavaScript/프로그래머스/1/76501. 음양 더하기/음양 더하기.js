ec1b107cad8d2c91d9e54293eb33bc0aaf7a49e7
Do Young <64531572+kimdoyoung1110@users.noreply.github.com>
2026-04-03 15:34:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,14 @@
+function solution(absolutes, signs) {
+    let result = 0;
+    
+    for(let i = 0;i<absolutes.length;i++){
+        if (!signs[i]){
+            result -= absolutes[i];
+        }
+        else{
+            result += absolutes[i];
+        }
+    }
+    
+    return result;
+}
